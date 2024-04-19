d3faa62785642e6dc1082ce4dc1d47e0cbec9790
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2024-04-19 17:14:35 -0400

    correctly casted args in __call__ for custom_schedule.py

--- custom_schedule.py
+++ custom_schedule.py
@@ -8,10 +8,15 @@ class custom_schedule(tf.keras.optimizers.schedules.LearningRateSchedule):
       self.d_model = tf.cast(self.d_model, tf.float32)
       self.warmup_steps = warmup_steps
 
+   # def __call__(self, step):
+   #    arg1 = tf.math.rsqrt(tf.cast(step, tf.float32))
+   #    step = tf.cast(step, tf.int64)
+   #    arg2 = step * (self.warmup_steps ** -1.5)
+   #    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+   
    def __call__(self, step):
       arg1 = tf.math.rsqrt(tf.cast(step, tf.float32))
-      step = tf.cast(step, tf.int64)
-      arg2 = step * (self.warmup_steps ** -1.5)
+      arg2 = tf.cast(step, tf.float32) * (self.warmup_steps ** -1.5)
       return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
    def get_config(self):
