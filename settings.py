61ddd23c5973012c807be73e66fc5bb7ecc425e2
Thilak C M <thilakcm@wireless-10-104-142-115.umd.edu>
2024-04-23 09:30:09 -0400

    Added images and model weights, updated settings

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ SHUFFLE_DIM = 512
 # Batch size
 BATCH_SIZE = 16 # 64
 # Numbers of training epochs
-EPOCHS = 20
+EPOCHS = 15
 
 # Reduce Dataset
 # If you want reduce number of train/valid images dataset, set 'REDUCE_DATASET=True'
@@ -25,11 +25,11 @@ EPOCHS = 20
 # Max number valid dataset images : 33432
 REDUCE_DATASET = True
 # Number of train images -> it must be a value between [1, 68363]
-NUM_TRAIN_IMG = 1000
+NUM_TRAIN_IMG = 20000
 # Number of valid images -> it must be a value between [1, 33432]
 # N.B. -> IMPORTANT : the number of images of the test set is given by the difference between 33432 and NUM_VALID_IMG values.
 # for instance, with NUM_VALID_IMG = 20000 -> valid set have 20000 images and test set have the last 13432 images.
-NUM_VALID_IMG = 200
+NUM_VALID_IMG = 5000
 # Data augumention on train set
 TRAIN_SET_AUG = True
 # Data augmention on valid set
