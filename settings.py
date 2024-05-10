b9795682d2a64ad43e6bc48f62e3098fb8381d93
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2024-05-09 22:54:08 -0400

    added files

--- settings.py
+++ settings.py
@@ -1,21 +1,21 @@
 # Desired image dimensions
 IMAGE_SIZE = (299, 299)
 # Max vocabulary size
-MAX_VOCAB_SIZE = 2000000 # 2000000
+MAX_VOCAB_SIZE = 1000000 # 2000000
 # Fixed length allowed for any sequence
-SEQ_LENGTH = 25 # 25
+SEQ_LENGTH = 15 # 25
 # Dimension for the image embeddings and token embeddings
 EMBED_DIM = 512
 # Number of self-attention heads
-NUM_HEADS = 12
+NUM_HEADS = 8
 # Per-layer units in the feed-forward network
 FF_DIM = 1024
 # Shuffle dataset dim on tf.data.Dataset
 SHUFFLE_DIM = 1 # 512
 # Batch size
-BATCH_SIZE = 32 # 64
+BATCH_SIZE = 24 # 64
 # Numbers of training epochs
-EPOCHS = 15
+EPOCHS = 20
 
 # Reduce Dataset
 # If you want reduce number of train/valid images dataset, set 'REDUCE_DATASET=True'
@@ -23,13 +23,13 @@ EPOCHS = 15
 #### COCO dataset
 # Max number train dataset images : 68363
 # Max number valid dataset images : 33432
-REDUCE_DATASET = False
+REDUCE_DATASET = True
 # Number of train images -> it must be a value between [1, 68363]
-NUM_TRAIN_IMG = 68363 # 68363
+NUM_TRAIN_IMG = 6000 # 68363
 # Number of valid images -> it must be a value between [1, 33432]
 # N.B. -> IMPORTANT : the number of images of the test set is given by the difference between 33432 and NUM_VALID_IMG values.
 # for instance, with NUM_VALID_IMG = 20000 -> valid set have 20000 images and test set have the last 13432 images.
-NUM_VALID_IMG = 20000 # 20000
+NUM_VALID_IMG = 200 # 20000
 # Data augumention on train set
 TRAIN_SET_AUG = True
 # Data augmention on valid set
