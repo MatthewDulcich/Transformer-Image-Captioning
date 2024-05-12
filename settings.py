c8047696215c4a9a1632bf05653808b2ef3cefa7
MatthewDulcich <61712797+MatthewDulcich@users.noreply.github.com>
2024-05-12 00:41:51 -0400

    added files before training

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # Desired image dimensions
 IMAGE_SIZE = (299, 299)
 # Max vocabulary size
-MAX_VOCAB_SIZE = 200000
+MAX_VOCAB_SIZE = 800000 # 2000000
 # Fixed length allowed for any sequence
-SEQ_LENGTH = 12
+SEQ_LENGTH = 20
 # Dimension for the image embeddings and token embeddings
 EMBED_DIM = 512
 # Number of self-attention heads
@@ -13,9 +13,9 @@ FF_DIM = 1024
 # Shuffle dataset dim on tf.data.Dataset
 SHUFFLE_DIM = 1 # 512
 # Batch size
-BATCH_SIZE = 32
+BATCH_SIZE = 32 # 64
 # Numbers of training epochs
-EPOCHS = 1
+EPOCHS = 20
 
 # Reduce Dataset
 # If you want reduce number of train/valid images dataset, set 'REDUCE_DATASET=True'
@@ -25,11 +25,11 @@ EPOCHS = 1
 # Max number valid dataset images : 33432
 REDUCE_DATASET = True
 # Number of train images -> it must be a value between [1, 68363]
-NUM_TRAIN_IMG = 200 # 68363
+NUM_TRAIN_IMG = 68363 # 68363
 # Number of valid images -> it must be a value between [1, 33432]
 # N.B. -> IMPORTANT : the number of images of the test set is given by the difference between 33432 and NUM_VALID_IMG values.
 # for instance, with NUM_VALID_IMG = 20000 -> valid set have 20000 images and test set have the last 13432 images.
-NUM_VALID_IMG = 60 # 20000
+NUM_VALID_IMG = 20000 # 20000
 # Data augumention on train set
 TRAIN_SET_AUG = True
 # Data augmention on valid set
